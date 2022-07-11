4c6dd8d8ab9354dae670f745a01e6b773ec2a1bd
dlslambot <106543696+dlslambot@users.noreply.github.com>
2022-07-11 16:04:28 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install python3 python3-pip python3-testresources
+
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
